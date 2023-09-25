24ac7e6ddc6e4b22c148e0f96f182f744d886983
darrida <darrida.py@gmail.com>
2023-09-25 14:39:03 -0500

    Numerous adjustments to satisfy ruff/black

--- setup.py
+++ setup.py
@@ -1,15 +1,15 @@
 from setuptools import setup
 
 setup(
-    name='pykeypass',
-    version='0.94',
-    py_modules=['pykeypass'],
+    name="pykeypass",
+    version="0.94",
+    py_modules=["pykeypass"],
     install_requires=[
-        'Click',
-        'pykeepass',
+        "Click",
+        "pykeepass",
     ],
-    entry_points='''
+    entry_points="""
         [console_scripts]
         pykeypass=pykeypass:cli
-    ''',
+    """,
 )
